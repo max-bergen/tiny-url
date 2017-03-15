46ebe63ce470f84908d268da7ab870efa79c1b32
max-bergen <maxbergen1234@gmail.com>
2017-03-15 19:22:15 +0000

    can delete urls from database object

---
+++ express-server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+const urlDatabase = {
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
+}
+
+function generateRandomString() {
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  let short = generateRandomString();
+  //res.send(short);
+  let long = req.body['longURL'];
+  urlDatabase[short] = long;
+  res.redirect('/urls/' + short);
+  console.log(urlDatabase);
+});
+
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.get('/urls/:id', (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+                       longURL: urlDatabase[req.params.id]  };
+  res.render('urls_show', templateVars);
+});
+
+app.get('/', (req, res) => {
+  res.end('welcome to TinyUrl');
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+})
+
+app.get('/hello', (req, res) => {
+  res.end('<html><body>Hello<b> World</b></body></html>\n');
+});
+
+app.listen(PORT, () => {
+  console.log(`hey guys, checking in on port: ${PORT}`);
+});
